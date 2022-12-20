bac686b576d97a2896830db509ebb15e9e9671e7
yellalena <yellalena@gmail.com>
2022-12-20 23:15:11 +0100

    day2 pt2 solution

--- 2022/day2/day2.py
+++ 2022/day2/day2.py
@@ -1,19 +1,27 @@
 with open("day2_input.txt", "r") as file:
-    strategy_input = file.readlines()
+    strategy_guide = [line.rstrip().split(" ") for line in file.readlines()]
 
-test_input = ["A Y", "B X", "C Z"]
+test_strategy_guide = [line.rstrip().split(" ") for line in ["A Y", "B X", "C Z"]]
 
-strategy_guide = [line.rstrip().split(" ") for line in strategy_input]
+choice_scores_map = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+result_scores_map = {1: 0, 0: 3, 2: 6}
 
-def get_total_score(strategy_guide: list):
-    choice_scores_map = {"A": 1, "B": 2, "C": 3,
-                     "X": 1, "Y": 2, "Z": 3}
-    result_scores_map = {1: 0, 0: 3, 2: 6}
-
+def get_total_score_v1(strategy_guide: list):
     total_score = 0
-    for game_round in strategy_guide:
-        winner = (3 + choice_scores_map[game_round[0]] - choice_scores_map[game_round[1]]) % 3
-        total_score += choice_scores_map[game_round[1]] + result_scores_map[winner]
+    for elf, me in strategy_guide:
+        winner = (3 + choice_scores_map[elf] - choice_scores_map[me]) % 3
+        total_score += choice_scores_map[me] + result_scores_map[winner]
     return total_score
 
-print(get_total_score(strategy_guide))
+print(get_total_score_v1(strategy_guide))
+
+result_scores_map= {"X": 0, "Y": 3, "Z": 6}
+
+def get_total_score_v2(strategy_guide: list):
+    total_score = 0
+    for elf, me in strategy_guide:
+        choice = (choice_scores_map[elf] + (choice_scores_map[me] - 2) + 2) % 3
+        total_score += choice + result_scores_map[me] + 1
+    return total_score
+
+print(get_total_score_v2(strategy_guide))
