9dc254b7522bd3c685634de7eec82e87320067a5
yellalena <yellalena@gmail.com>
2022-12-26 01:53:06 +0100

    added day1 solution on C#

---
+++ 2022/day1/day1_cs/day1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Day1
+{
+    static void Main(string[] args)
+    {
+        string input_lines = File.ReadAllText("day1_input.txt");
+
+        var calorieBatches = input_lines.Split("\n\n").ToList();
+        var sumsList = new List<int>();
+
+        foreach (var batch in calorieBatches)
+        {
+            sumsList.Add(batch.Split("\n").ToList().ConvertAll(int.Parse).Sum());
+        }
+
+        // part 1
+        Console.WriteLine(GetMaxSumOfCalories(sumsList, 1));
+
+        // part 2
+        Console.WriteLine(GetMaxSumOfCalories(sumsList, 3));
+    }
+
+    private static int GetMaxSumOfCalories(List<int> calorieSumsList, int nTopOfElves)
+    {
+        calorieSumsList.Sort();
+        calorieSumsList.Reverse();
+
+        return calorieSumsList.GetRange(0, nTopOfElves).Sum();
+    }
+}
