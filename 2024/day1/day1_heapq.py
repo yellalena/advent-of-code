ccf82c6b8b2f4dd3461a284afb68a3ad724d2d2c
yellalena <yellalena@gmail.com>
2024-12-01 21:08:06 +0100

    day1: updated heapq solution

--- 2024/day1/day1_heapq.py
+++ 2024/day1/day1_heapq.py
@@ -1,18 +1,15 @@
 
-from heapq import heappop, heappush
+from heapq import heapify, heappop, heappush
 from typing import Counter
 from utils import read_input
 
 
 input_lines = read_input("2024/day1")
 
-list1 = []
-list2 = []
-for line in input_lines.split("\n"):
-    a, b = map(int, line.split("   "))
-    heappush(list1, a)
-    heappush(list2, b)
+list1, list2 = map(list, zip(*(map(int, line.split("   ")) for line in input_lines.split("\n"))))
 
+heapify(list1)
+heapify(list2)
 list2_counter = Counter(list2)
 
 total_distance = 0
