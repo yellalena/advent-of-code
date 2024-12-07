b79e1f305d36d8201599b950a466adbf19341f99
yellalena <yellalena@gmail.com>
2024-12-07 19:44:43 +0100

    day3 pt 2

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -4,14 +4,30 @@ import re
 
 input = read_input("2024/day3")
 
-result = re.findall("mul\\(\\d{1,3},\\d{1,3}\\)", input)
-
 def apply_mul_instruction(instruction):
     a, b = re.findall("\\((\\d+),(\\d+)\\)", instruction)[0]
     return int(a) * int(b)
 
-total = 0
-for i in result:
-    total += apply_mul_instruction(i)
+def count_total_pt1(input):
+    result = re.findall("mul\\(\\d{1,3},\\d{1,3}\\)", input)
+    total = 0
+    for i in result:
+        total += apply_mul_instruction(i)
+    return total
 
-print("Total sum: ", total)
+def count_total_pt2(input):
+    result = re.findall("(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\'t\\(\\))", input)
+    total = 0
+    do = True
+    for i in result:
+        if i == "do()":
+            do = True
+        elif i == "don't()":
+            do = False
+
+        if do and i.startswith("mul"):
+            total += apply_mul_instruction(i)
+    return total
+
+print("Total sum 1: ", count_total_pt1(input))
+print("Total sum 2: ", count_total_pt2(input))
