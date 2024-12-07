759d1601b51e55eff8ecd6cbdc3a904922e4a14d
yellalena <yellalena@gmail.com>
2024-12-07 19:48:35 +0100

    improved code for day 3

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -4,28 +4,29 @@ import re
 
 input = read_input("2024/day3")
 
+MUL_PATTERN = "\\((\\d+),(\\d+)\\)"
+INSTRUCTION_PATTERN_1 = "mul\\(\\d{1,3},\\d{1,3}\\)"
+INSTRUCTION_PATTERN_2 = "(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\'t\\(\\))"
+
 def apply_mul_instruction(instruction):
-    a, b = re.findall("\\((\\d+),(\\d+)\\)", instruction)[0]
-    return int(a) * int(b)
+    a, b = map(int, MUL_PATTERN.search(instruction).groups())
+    return a * b
 
 def count_total_pt1(input):
-    result = re.findall("mul\\(\\d{1,3},\\d{1,3}\\)", input)
-    total = 0
-    for i in result:
-        total += apply_mul_instruction(i)
-    return total
+    instructions = re.findall(INSTRUCTION_PATTERN_1, input)
+    return sum(apply_mul_instruction(i) for i in instructions)
 
 def count_total_pt2(input):
-    result = re.findall("(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\'t\\(\\))", input)
+    instructions = re.findall(INSTRUCTION_PATTERN_2, input)
     total = 0
     do = True
-    for i in result:
+    for i in instructions:
         if i == "do()":
             do = True
         elif i == "don't()":
             do = False
 
-        if do and i.startswith("mul"):
+        elif do and i.startswith("mul"):
             total += apply_mul_instruction(i)
     return total
 
